59d8530091dfdb491c009f30e9d7909c0a026dc7
Ilya Dmitrichenko <ilya.dmitrichenko@cosm.com>
2013-01-05 01:47:15 +0000

    It almost does it in Cosm data format!

--- listen.py
+++ listen.py
@@ -1,11 +1,16 @@
+import json
 import SocketServer
 
 class DatagramHandler(SocketServer.BaseRequestHandler):
-  def handle(self):
-    data = self.request[0].strip()
-    socket = self.request[1]
-    print data
+    def handle(self):
+        samp = self.request[0].strip().strip(';').split(' ')
+        #XXX: should we need to raise here (if samp.__len__ =! 31)?
+        chan = samp[0]
+        samp.__delitem__(0)
+        datastreams = [{'id':chan+'.'+str(i), 'current_value':x} for i,x in enumerate(samp)]
+        print json.dumps(datastreams)
+
 
 if __name__ == "__main__":
-  server = SocketServer.UDPServer(('localhost', 3000), DatagramHandler)
-  server.serve_forever()
+    server = SocketServer.UDPServer(('localhost', 3000), DatagramHandler)
+    server.serve_forever()
